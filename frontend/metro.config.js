21de010abc5841cdacc2fc57e6403157e997c4a3
rakesh1-cj <corpjack.rakesh1@gmail.com>
2025-12-27 07:51:15 +0530

    setup svg render

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -3,5 +3,18 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+
+// 👇 Add SVG support
+config.transformer = {
+    ...config.transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  };
+  
+  config.resolver = {
+    ...config.resolver,
+    assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...config.resolver.sourceExts, 'svg'],
+  };
+
 module.exports = withNativeWind(config, { input: './src/styles/global.css' });
 
